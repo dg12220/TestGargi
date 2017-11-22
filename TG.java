4d6649788c5f380c414d6563481d6cfc2878bcdc
Dhananjay Sudhakar Gatade <DS00359937@techmahindra.com>
2017-11-22 15:32:11 +0530

    New Project

---
+++ TG.java
@@ -0,0 +1,11 @@
+public class TG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My name is Gargi");
+		System.out.println("I stay in Pune - 21");
+		
+		}
+
+}
